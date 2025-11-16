3ae53db9cc218bddae55ed8bd4ad28b4796e98a4
Pavel Vedernikov <pvedernikov@gmail.com>
2025-11-16 19:22:28 +0100

    Blind75: 268. Missing Number

--- LeetCodeBlind75.cs
+++ LeetCodeBlind75.cs
@@ -367,6 +367,22 @@ public static class LeetCodeBlind75
 
         return false;
     }
+
+    // #11
+    // 268. Missing Number
+    // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+    public static int MissingNumber(int[] nums)
+    {
+        var n = nums.Length;
+        var result = ((n + 1) * n) / 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            result -= nums[i];
+        }
+
+        return result;
+    }
 }
 
 public class Node133
