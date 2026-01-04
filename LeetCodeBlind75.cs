0d2f6f4b73c79aa7cc650700028642dbf715e0a9
Pavel Vedernikov <pvedernikov@gmail.com>
2026-01-04 19:04:51 +0100

    Blind 75: 62. Unique Paths

--- LeetCodeBlind75.cs
+++ LeetCodeBlind75.cs
@@ -1485,7 +1485,68 @@ public static class LeetCodeBlind75
     #endregion
 
     // #37
+    // 62. Unique Paths
+    // There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
+    // The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+    // Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+    // O(m * n) time complexity
+    //
+    // Важно: Можно решить через формулу C(m+n-2, m-1) или C(m+n-2, n-1), например (m + n - 2)! / ((m - 1)! * (n - 1)!) 
+    // При этом важно следить, чтобы не было переполнения при вычислении факториала
+    // Так что возможно DP тут предпочтительнее
+    #region 62. Unique Paths
 
+    // First attempt: DP + Memoization, recursive
+    public static int UniquePathsR(int m, int n, Dictionary<(int, int), int> cache = null)
+    {
+        if (n <= 1 || m <= 1) return 1;
+
+        if (n == 2) return m;
+        if (m == 2) return n;
+
+        var key = (Math.Min(m, n), Math.Max(m, n));
+        if (cache == null)
+        {
+            cache = new Dictionary<(int, int), int>();
+        }
+        else
+        {
+            if (cache.ContainsKey(key))
+            {
+                return cache[key];
+            }
+        }
+
+        var result = UniquePathsR(m - 1, n, cache) + UniquePathsR(m, n - 1, cache);
+        cache[key] = result;
+        return result;
+    }
+
+    // Second attempt: DP + Memoization, iterative
+    public static int UniquePaths(int m, int n)
+    {
+        if (n <= 1 || m <= 1) return 1;
+
+        if (n == 2) return m;
+        if (m == 2) return n;
+
+        var cache = new int[m, n];
+
+        for (int i = 0; i < m; i++) { cache[i, 0] = 1; }
+        for (int j = 1; j < n; j++) { cache[0, j] = 1; }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                cache[i, j] = cache[i - 1, j] + cache[i, j - 1];
+            }
+        }
+
+        return cache[m - 1, n - 1];
+    }
+
+    #endregion
     // #38
 
     // #39
