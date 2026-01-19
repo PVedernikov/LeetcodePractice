2b5d0a32d2d0f15e80c64d54079f508e007cfdb7
Pavel Vedernikov <pvedernikov@gmail.com>
2026-01-19 23:46:33 +0100

    Blind 75: 91. Decode Ways

--- LeetCodeBlind75.cs
+++ LeetCodeBlind75.cs
@@ -2111,8 +2111,63 @@ public static class LeetCodeBlind75
     #endregion
 
     // #57
-    // 
-    #region
+    // 91. Decode Ways
+    // You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+    // "1" -> 'A'
+    // "2" -> 'B'
+    // ...
+    // "25" -> 'Y'
+    // "26" -> 'Z'
+    // However, while decoding the message, you realize that there are many different ways you can decode
+    // the message because some codes are contained in other codes ("2" and "5" vs "25").
+    // For example, "11106" can be decoded into:
+    // "AAJF" with the grouping (1, 1, 10, 6)
+    // "KJF" with the grouping (11, 10, 6)
+    // The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+    // Note: there may be strings that are impossible to decode.
+    // Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+    // DP + Memoization, recursive
+    // O(n) time complexity
+    #region 91. Decode Ways
+    public static int NumDecodings(string s)
+    {
+        return NumDecodings(s, 0, new Dictionary<int, int>());
+    }
+
+    public static int NumDecodings(string s, int i, Dictionary<int, int> cache)
+    {
+        if (cache.ContainsKey(i)) return cache[i];
+        var n = s.Length;
+        if (i >= n) return 1;
+        if (s[i] == '0') return 0;
+        if (i == n - 1) return 1;
+        if (s[i] == '1')
+        {
+            if (s[i + 1] == '0')
+            {
+                cache[i] = NumDecodings(s, i + 2, cache);
+                return cache[i];
+            }
+            cache[i] = NumDecodings(s, i + 2, cache) + NumDecodings(s, i + 1, cache);
+            return cache[i];
+        }
+        if (s[i] == '2')
+        {
+            if (s[i + 1] >= '1' && s[i + 1] <= '6')
+            {
+                cache[i] = NumDecodings(s, i + 2, cache) + NumDecodings(s, i + 1, cache);
+                return cache[i];
+            }
+            if (s[i + 1] == '0')
+            {
+                cache[i] = NumDecodings(s, i + 2, cache);
+                return cache[i];
+            }
+        }
+
+        cache[i] = NumDecodings(s, i + 1, cache);
+        return cache[i];
+    }
     #endregion
 
     // #58
