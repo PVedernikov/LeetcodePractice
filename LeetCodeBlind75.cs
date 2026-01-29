19ee4fa99d3715886ba53254d16f315a03e66c7c
Pavel Vedernikov <pvedernikov@gmail.com>
2026-01-29 22:20:57 +0100

    Blind 75: 121. Best Time to Buy and Sell Stock

--- LeetCodeBlind75.cs
+++ LeetCodeBlind75.cs
@@ -2702,8 +2702,25 @@ public static class LeetCodeBlind75
     #endregion
 
     // #74
-    // 
-    #region
+    // 121. Best Time to Buy and Sell Stock
+    // You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+    #region 121. Best Time to Buy and Sell Stock
+    public static int MaxProfit(int[] prices)
+    {
+        var result = 0;
+        var min = int.MaxValue;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < min)
+            {
+                min = prices[i];
+            }
+            result = Math.Max(result, prices[i] - min);
+        }
+        return result;
+    }
     #endregion
 
     // #75
