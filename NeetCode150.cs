66a6dcadd35cbe3c75fcf52969c86a5d58d93e64
Pavel Vedernikov <pvedernikov@gmail.com>
2026-03-10 01:16:06 +0100

    746. Min Cost Climbing Stairs - small refactoring

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -66,7 +66,7 @@ public class NeetCode150
     #endregion
 
     #region 1-D Dynamic Programming
-    
+
     // 746. Min Cost Climbing Stairs
     // You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
     // You can either start from the step with index 0, or the step with index 1.
@@ -80,9 +80,8 @@ public class NeetCode150
         for (int i = 2; i < n; i++)
         {
             var minI = cost[i] + Math.Min(min0, min1);
-            var tmp = min1;
+            min0 = min1;
             min1 = minI;
-            min0 = tmp;
         }
 
         return Math.Min(min0, min1);
