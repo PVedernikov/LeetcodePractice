607b1ee80cc3750b5adef0e487e1014b97673948
Pavel Vedernikov <pvedernikov@gmail.com>
2026-03-23 01:36:23 +0100

    NeetCode 150: 55. Jump Game

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -480,14 +480,39 @@ public class NeetCode150
 
     #region Greedy
 
+
+    // 55. Jump Game
+    // You are given an integer array nums. You are initially positioned at the array's first index,
+    // and each element in the array represents your maximum jump length at that position
+    // Return true if you can reach the last index, or false otherwise.
+    #region 55. Jump Game
+    public bool CanJump(int[] nums)
+    {
+        var n = nums.Length;
+        if (n <= 1) return true;
+
+        var maxIndex = 0;
+
+        for (int i = 0; i <= maxIndex; i++)
+        {
+            maxIndex = Math.Max(maxIndex, i + nums[i]);
+            if (maxIndex >= n - 1) return true;
+        }
+
+        return false;
+    }
+    #endregion
+
+
     // 45. Jump Game II
     // You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
     // Each element nums[i] represents the maximum length of a forward jump from index i.
     // In other words, if you are at index i, you can jump to any index (i + j) where:
     // 0 <= j <= nums[i] and
     // i + j < n
+    // Return the minimum number of jumps to reach index n - 1. The test cases are generated such that you can reach index n - 1.
     #region 45. Jump Game II
-    
+
     // DP solution
     // NOT OPTIMAL
     // O(n^2) time complexity
