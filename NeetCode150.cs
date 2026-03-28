dd55effe5baaba50783765c76425d73dc2bad125
Pavel Vedernikov <pvedernikov@gmail.com>
2026-03-28 21:39:15 +0100

    NeetCode 150: 704. Binary Search

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -181,6 +181,38 @@ public class NeetCode150
 
     #endregion
 
+
+    #region Binary Search
+
+    // 704. Binary Search
+    // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+    // If target exists, then return its index. Otherwise, return -1.
+    // You must write an algorithm with O(log n) runtime complexity.
+    #region 704. Binary Search
+    public int Search(int[] nums, int target)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+        while (l <= r)
+        {
+            var mid = l + (r - l) / 2;
+            if (target == nums[mid]) return mid;
+            if (target < nums[mid])
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+    #endregion
+
+    #endregion
+
     #region Trees
     // 543. Diameter of Binary Tree
     // Given the root of a binary tree, return the length of the diameter of the tree.
