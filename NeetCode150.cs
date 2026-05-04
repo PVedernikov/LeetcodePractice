35bb8d7b1363ddc4ad3e35fcef36bcc1c74a252c
Pavel Vedernikov <pvedernikov@gmail.com>
2026-05-04 23:08:55 +0200

    NeetCode 150: 684. Redundant Connection

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -1338,6 +1338,52 @@ public class NeetCode150
         }
     }
     #endregion
+
+    // 684. Redundant Connection
+    // In this problem, a tree is an undirected graph that is connected and has no cycles.
+    // You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added.
+    // The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed.
+    // The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+    // Return an edge that can be removed so that the resulting graph is a tree of n nodes.
+    // If there are multiple answers, return the answer that occurs last in the input.
+    #region 684. Redundant Connection
+
+    // first attempt, O(n^2) time complexity, O(n) space complexity
+    // NOT OPTIMAL
+    // Идея: используем массив unionFind, где unionFind[i] - это родитель вершины i. Изначально каждый элемент является своим родителем.
+    // Мы схлопываем весь путь до одной вершитны, т.е. все вершины, которые принадлежат одной компоненте связности, будут иметь одного родителя.
+    // Изначально это не так, но когда мы обрабатываем ребра, мы объединяем компоненты связности.
+    // Когда мы обрабатываем ребро (a, b), если unionFind[a] == unionFind[b], значит a и b уже в одной компоненте связности, и это ребро лишнее, возвращаем его.
+    // Иначе, объединяем компоненты связности, т.е. все вершины, у которых родитель unionFind[b], теперь будут иметь родителя unionFind[a].
+    public int[] FindRedundantConnection(int[][] edges)
+    {
+        var n = edges.Length;
+        var unionFind = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            unionFind[i] = i;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            var a = edges[i][0];
+            var b = edges[i][1];
+
+            if (unionFind[a] == unionFind[b]) return edges[i];
+            var parentA = unionFind[a];
+            var parentB = unionFind[b];
+            for (int j = 1; j <= n; j++)
+            {
+                if (unionFind[j] == parentB)
+                {
+                    unionFind[j] = parentA;
+                }
+            }
+        }
+
+        return null;
+    }
+    #endregion
     #endregion
 
     #region Advanced Graphs
