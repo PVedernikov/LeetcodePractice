f57e5a121829bb2a845136908d84e15845d4d462
Pavel Vedernikov <pvedernikov@gmail.com>
2026-05-07 01:19:21 +0200

    NeetCode 150: 207. Course Schedule

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -1339,6 +1339,58 @@ public class NeetCode150
     }
     #endregion
 
+    // 207. Course Schedule
+    // There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+    // You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+    // - For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    // Return true if you can finish all courses. Otherwise, return false.
+    #region 207. Course Schedule
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        var adj = new List<int>[numCourses];
+        for (int i = 0; i < prerequisites.Length; i++)
+        {
+            var a = prerequisites[i][0];
+            var b = prerequisites[i][1];
+            if (adj[a] is null)
+            {
+                adj[a] = new List<int>();
+            }
+            adj[a].Add(b);
+        }
+
+        var finished = new int[numCourses]; // 0 - not processed, 1 - visited, 2 - finished
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (finished[i] == 2) continue;
+
+            if (!CanFinish(i))
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+        bool CanFinish(int i)
+        {
+            if (finished[i] == 2) return true;
+            if (finished[i] == 1) return false;
+
+            finished[i] = 1;
+            if (adj[i] is not null && adj[i].Count > 0)
+            {
+                foreach (int b in adj[i])
+                {
+                    if (!CanFinish(b)) return false;
+                }
+            }
+            finished[i] = 2;
+            return true;
+        }
+    }
+    #endregion
+
     // 210. Course Schedule II
     // There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
     // You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
