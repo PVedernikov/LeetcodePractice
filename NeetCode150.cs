e0803c7b8119893d99fba337a7096393e0ec8d5a
Pavel Vedernikov <pvedernikov@gmail.com>
2026-05-18 23:26:42 +0200

    NeetCode 150: 1899. Merge Triplets to Form Target Triplet

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -2048,6 +2048,41 @@ public class NeetCode150
         return true;
     }
     #endregion
+
+    // 1899. Merge Triplets to Form Target Triplet
+    // A triplet is an array of three integers. You are given a 2D integer array triplets, where triplets[i] = [ai, bi, ci] describes the ith triplet.
+    // You are also given an integer array target = [x, y, z] that describes the triplet you want to obtain.
+    // To obtain target, you may apply the following operation on triplets any number of times (possibly zero):
+    // - Choose two indices (0-indexed) i and j (i != j) and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
+    //   - For example, if triplets[i] = [2, 5, 3] and triplets[j] = [1, 7, 5], triplets[j] will be updated to [max(2, 1), max(5, 7), max(3, 5)] = [2, 7, 5].
+    // Return true if it is possible to obtain the target triplet [x, y, z] as an element of triplets, or false otherwise.
+    #region 1899. Merge Triplets to Form Target Triplet
+    public bool MergeTriplets(int[][] triplets, int[] target)
+    {
+        var n = triplets.Length;
+
+        var a = false;
+        var b = false;
+        var c = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            var t = triplets[i];
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2])
+                continue;
+
+            if (t[0] == target[0]) a = true;
+            if (t[1] == target[1]) b = true;
+            if (t[2] == target[2]) c = true;
+
+            if (a && b && c) return true;
+        }
+
+        return false;
+    }
+    #endregion
+
+
     // 678. Valid Parenthesis String
     // Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
     // The following rules define a valid string:
