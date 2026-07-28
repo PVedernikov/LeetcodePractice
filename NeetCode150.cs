70952205a7131967d3406424b341ccbcd7eaadda
Pavel Vedernikov <pvedernikov@gmail.com>
2026-07-29 01:30:30 +0200

    NeetCode 150: 167. Two Sum II - Input Array Is Sorted

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -393,6 +393,39 @@ public class NeetCode150
     }
     #endregion
 
+    // 167. Two Sum II - Input Array Is Sorted
+    // Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+    // Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+    // Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+    // The tests are generated such that there is exactly one solution. You may not use the same element twice.
+    // Your solution must use only constant extra space.
+    // O(1) - extra space required
+    #region 167. Two Sum II - Input Array Is Sorted
+    public int[] TwoSum167(int[] numbers, int target)
+    {
+        var l = 0;
+        var r = numbers.Length - 1;
+        while (l < r)
+        {
+            var sum = numbers[l] + numbers[r];
+            if (sum == target)
+            {
+                return new int[] { l + 1, r + 1 };
+            }
+
+            if (sum < target)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return [];
+    }
+    #endregion
+
     // 15. 3Sum
     // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
     // Notice that the solution set must not contain duplicate triplets.
