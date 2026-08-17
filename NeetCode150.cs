24efb89270c334104ce94d8446427f867b7b8533
Pavel Vedernikov <pvedernikov@gmail.com>
2026-08-17 22:50:07 +0200

    NeetCode 150: 295. Find Median from Data Stream

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -3273,6 +3273,65 @@ public class NeetCode150
     }
     #endregion
 
+    // 295. Find Median from Data Stream
+    // The median is the middle value in an ordered integer list.
+    // If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+    // - For example, for arr = [2,3,4], the median is 3.
+    // - For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+    // Implement the MedianFinder class:
+    // - MedianFinder() initializes the MedianFinder object.
+    // - void addNum(int num) adds the integer num from the data stream to the data structure.
+    // - double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+    // HARD
+    #region 295. Find Median from Data Stream
+    // Идея: храним две кучи (min и max), которые делят весь поток элементов пополам
+    // Если элементов четное количество, то обе кучи содержат равное количество элементов
+    // Если нечетное, то в левой куче на 1 больше
+    public class MedianFinder
+    {
+        private PriorityQueue<int, int> left;
+        private PriorityQueue<int, int> right;
+
+        public MedianFinder()
+        {
+            left = new PriorityQueue<int, int>();
+            right = new PriorityQueue<int, int>();
+        }
+
+        public void AddNum(int num)
+        {
+            if (right.Count > 0 && num > right.Peek()) // добавляем либо вправо, либо в лево, в зависимости от значения
+            {
+                right.Enqueue(num, num);
+            }
+            else
+            {
+                left.Enqueue(num, -num);
+            }
+
+            while (left.Count < right.Count) // Балансируем кучи
+            {
+                var val = right.Dequeue();
+                left.Enqueue(val, -val);
+            }
+
+            while (left.Count - right.Count > 1) // Балансируем кучи
+            {
+                var val = left.Dequeue();
+                right.Enqueue(val, val);
+            }
+        }
+
+        public double FindMedian()
+        {
+            return left.Count == right.Count
+                ? ((double)left.Peek() + (double)right.Peek()) / 2
+                : (double)left.Peek();
+
+        }
+    }
+    #endregion
+
     #endregion
 
     #region Backtracking
