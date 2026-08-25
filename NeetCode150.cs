f2a904a58d4db95a318e2b6f293b1712c93b8433
Pavel Vedernikov <pvedernikov@gmail.com>
2026-08-25 21:10:31 +0200

    NeetCode 150: 91. Decode Ways

--- NeetCode150.cs
+++ NeetCode150.cs
@@ -5211,6 +5211,55 @@ public class NeetCode150
     }
     #endregion
 
+    // 91. Decode Ways
+    // You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+    // "1" -> 'A'
+    // "2" -> 'B'
+    // ...
+    // "25" -> 'Y'
+    // "26" -> 'Z'
+    // However, while decoding the message, you realize that there are many different ways you can decode the message
+    // because some codes are contained in other codes ("2" and "5" vs "25").
+    // For example, "11106" can be decoded into:
+    // - "AAJF" with the grouping (1, 1, 10, 6)
+    // - "KJF" with the grouping (11, 10, 6)
+    // - The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+    // Note: there may be strings that are impossible to decode.
+    // Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+    // The test cases are generated so that the answer fits in a 32-bit integer.
+    #region 91. Decode Ways
+    public int NumDecodings(string s)
+    {
+        var n = s.Length;
+        var cache = new int[n];
+        for (int i = 0; i < n; i++) cache[i] = -1;
+
+        return ways(0);
+
+        int ways(int i)
+        {
+            if (i >= n) return 1;
+            if (cache[i] >= 0) return cache[i];
+            if (s[i] == '0')
+            {
+                cache[i] = 0;
+                return cache[i];
+            }
+
+            if (i < n - 1 && (s[i] == '1'
+                || (s[i] == '2' && s[i + 1] >= '0' && s[i + 1] <= '6')
+            ))
+            {
+                cache[i] = ways(i + 1) + ways(i + 2);
+                return cache[i];
+            }
+
+            cache[i] = ways(i + 1);
+            return cache[i];
+        }
+    }
+    #endregion
+
     // 416. Partition Equal Subset Sum
     // Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
     #region 416. Partition Equal Subset Sum
